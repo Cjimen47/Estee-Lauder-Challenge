ff06fdd96c8c73fff5856c86dd6acd6730e23343
Coral Jimenez-Gudino <coraljimenez720@gmail.com>
2023-06-21 20:34:54 -0700

    Added Beginning

--- Estee Lauder Challeneg/ContentView.swift
+++ Estee Lauder Challeneg/ContentView.swift
@@ -6,21 +6,57 @@
 //
 
 import SwiftUI
-
-struct ContentView: View {
+import UIKit
+    
+struct BeginningView: View {
+   
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationStack{
+            ZStack{
+                //Trying to set up a general frame
+                Color(red: 171/255, green: 237/255, blue: 130/255)
+                    .ignoresSafeArea()
+                Rectangle()
+                    .strokeBorder(Color(red: 255/255, green: 201/255, blue: 218/255), lineWidth: 50)
+                    .frame(width:450, height: 900)
+                    
+                
+                VStack {
+                    Image("first")
+                        .resizable(resizingMode: .stretch)
+                        .frame(width: 200.0, height: 100.0)
+                        .padding()
+                        .cornerRadius(80)
+                        
+                        
+                        
+                    Text("You Want to Become Even More\n    Stunning Than You Already\n             Are Gorgeous?!")
+                    Image("sec")
+                        .resizable(resizingMode: .stretch)
+                        .frame(width: 200.0, height: 100.0)
+                        .padding()
+                        .cornerRadius(80)
+                    
+                    Text("You Want To Do it Using Ingredients\n     That Don't Sound Like They're\n                 Melt Your Face Off?")
+                    
+                    Image("third").resizable(resizingMode: .stretch)
+                        .frame(width: 200.0, height: 100.0)
+                        .padding()
+                        .cornerRadius(80)
+                    
+                    /*Button(action:{}, label: {NavigationLink(destination:MidView())
+                        {Text("Click Here to learn more lol")}
+                    })*/
+                }
+                .padding()
+            }
         }
-        .padding()
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
+struct BeginningView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        BeginningView()
     }
 }
--- Estee Lauder Challeneg/Estee_Lauder_ChallenegApp.swift
+++ Estee Lauder Challeneg/Estee_Lauder_ChallenegApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Estee_Lauder_ChallenegApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BeginningView()
         }
     }
 }
