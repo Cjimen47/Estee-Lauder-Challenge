9848e054764101dead6e759bda1be24988aafe13
Coral Jimenez-Gudino <coraljimenez720@gmail.com>
2023-06-21 20:30:26 -0700

    Initial Commit

---
+++ Estee Lauder Challeneg/Estee_Lauder_ChallenegApp.swift
@@ -0,0 +1,17 @@
+//
+//  Estee_Lauder_ChallenegApp.swift
+//  Estee Lauder Challeneg
+//
+//  Created by Coral Jimenez-Gudino on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Estee_Lauder_ChallenegApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
